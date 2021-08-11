5a38310cb64cfcb446401ee087bf45673682b07f
katorly <1649643021@qq.com>
2021-08-11 13:47:22 +0800

    2021.8.11 13:47

---
+++ CleanJunkFiles.bat
@@ -0,0 +1,22 @@
+@echo off
+echo Cleaning junk files, please wait...
+del /f /s /q %systemdrive%\*.tmp
+del /f /s /q %systemdrive%\*._mp
+del /f /s /q %systemdrive%\*.gid
+del /f /s /q %systemdrive%\*.chk
+del /f /s /q %systemdrive%\*.old
+del /f /s /q %systemdrive%\recycled\*.*
+del /f /s /q %windir%\*.bak
+del /f /s /q %windir%\prefetch\*.*
+rd /s /q %windir%\temp & md %windir%\temp
+del /f /q %userprofile%\cookies\*.*
+del /f /q %userprofile%\recent\*.*
+del /f /s /q "%userprofile%\Local Settings\Temporary Internet Files\*.*"
+del /f /s /q "%userprofile%\Local Settings\Temp\*.*"
+del /f /s /q "%userprofile%\recent\*.*"
+del /f /s /q "%windir%\auto.exe"
+del /f /s /q "%windir%%\autorun.inf"
+del /f /s /q readme.eml"
+del /f /s /q runouce.exe
+echo Junk files cleaned!
+echo. & pause
---
+++ README.md
@@ -0,0 +1,10 @@
+# CleanJunkFiles
+[![License](https://img.shields.io/badge/license-GPLv3-blue?style=for-the-badge)](https://www.gnu.org/licenses/gpl-3.0.html) ![Pull Requests](https://img.shields.io/github/issues-pr-closed/katorlys/CleanJunkFiles?style=for-the-badge) ![Issues](https://img.shields.io/github/issues-closed/katorlys/CleanJunkFiles?style=for-the-badge)
+
+## Introduction
+Automatically cleans the junk files in your PC. Improve your PC's operating efficiency by just one click.<br>
+
+<b>Only spported on Windows.</b>
+
+## Usage
+Double-click `CleanJunkFiles.bat`.<br>
